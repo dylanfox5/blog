55ff45aba6bb485c6f44075b9ab1d7537b49b1a1
Dylan Fox <dylan.fox.kc@gmail.com>
2022-05-14 20:52:24 -0500

    change theme and config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `The FoxPad`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Dylan Fox`,
+      summary: `who lives and works in Kansas City developing analytics and writing software for Evolytics.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `dfox_5`,
     },
   },
   plugins: [
